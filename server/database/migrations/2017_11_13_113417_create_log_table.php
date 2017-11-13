05aeaaea346c454f42dee819e3985fc6e9795137
Nuno Anselmo <n.anselmo@hotmail.com>
2017-11-13 16:15:38 +0000

    Adds migrations

---
+++ server/database/migrations/2017_11_13_113417_create_log_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateLogTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('log', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('file')->references('id')->on('file');
+            $table->unsignedInteger('user')->references('id')->on('user');
+            $table->timestamp('date')->useCurrent();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('log');
+    }
+}
---
+++ server/database/migrations/2017_11_13_113426_create_access_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateAccessTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('access', function (Blueprint $table) {
+            $table->primary(['user', 'file']);
+            $table->string('user')->references('id')->on('user');
+            $table->string('file')->references('id')->on('file');
+            $table->string('key');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('access');
+    }
+}
